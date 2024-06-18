71ce0cd7a97622045c9a83d52ecd55677fc677a2
Liz Mutua <70329767+lizmutua@users.noreply.github.com>
2024-06-18 23:45:02 +0300

    string

---
+++ exercise string.py
@@ -0,0 +1,41 @@
+#Test="Monty Python"
+#print(Test[0])
+#print(Test[-1])
+#print(len(Test))
+#print(Test[len(Test)-1])
+#print(Test[:5])
+
+Test2="homebody"
+#print(Test2[:4])
+#print(Test2[4:])
+
+#def half(text):
+    #middle = len(text) // 2
+    #return text[0:middle]
+
+
+#data = input("Enter a String : ")
+#print("First Half of String is : ", half(data))
+
+#def half(test4):
+   # middle=len(test4)//2
+   # return test4[middle:] 
+
+#data2 = input("Enter a string:")
+#print("last half of the string is : ", half(data2))
+
+#def middle(test5):
+	middle = len(test5) //2
+	return test5[middle]
+#Data3 = input("Enter a String : ") 
+#print("middle of the String is : ", middle(Data3))
+ 
+
+#def half(test6):
+#middle = len(text) // 2
+# return text[0:middle]
+
+
+#data = input("Enter a String : ")
+#print("First Half of String is : ", half(data))
+
