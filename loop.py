75faf833d14497d36f914ffe5d3006d3e0e14c72
Liz Mutua <70329767+lizmutua@users.noreply.github.com>
2024-06-18 23:46:04 +0300

    loop

---
+++ loop.py
@@ -0,0 +1,29 @@
+# character='ABCDEFGHIJKLMNOPQR'
+# for I in character:
+#     print(I)
+
+# list=[1,2,3,4,5,6,7,8,9]
+# newlist=[]
+# for item in iter(list):
+#     newvalue= item*2
+#     newlist.append(newvalue)
+#     print(newlist)
+
+
+    
+# list=[1,2,3,4,5,6,7,8,9]
+# newlist=[]
+# for item in iter(list):
+#     newvalue= item*2
+#     newlist.append(newvalue)
+# print(newlist)
+
+
+# for i in range(2,30):
+#     print(i)
+
+# for i in range(2,30,3):
+#     print(i)
+
+for i in range(30,2,-2):
+    print(i)
