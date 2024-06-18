f75acc1554b65404770f2e44e03a8cf1f49f4bf0
Liz Mutua <70329767+lizmutua@users.noreply.github.com>
2024-06-18 23:39:24 +0300

    Practice 1
    python and  word

---
+++ execise.py
@@ -0,0 +1,30 @@
+#print("\ta\tb\tc") 
+
+#print("\\\\")
+
+#print("'")
+
+#print("\"\"\"")
+
+#print("C:\nin\the downward spiral")
+
+#/ \ // \\ /// \\\
+
+#print('/\t \\t //\t \\\t ///\t \\\')
+      
+print("/\t\\\t//\t \\\\\t ///\t\\\\\\")
+ 
+#print(r"/ \ // \\ /// \\\ ")
+
+#print("A \"quoted\" String is \n 'much' better if you learn\nthe rules of \"escape sequences.\n\" Also, \"\"represents an empty String.\nDon't forget: use \\\" instead of \" !\n'' is not the same as \" ")
+
+#print("This quote is from\n Irish poet Oscar Wilde:\n \"Music makes one feel so romantic\n- at least it always gets on one's nerves –\n which is the same thing nowadays." )
+
+print(9 / 5)
+print(695 % 20)
+print(7 + (6 * 5))
+print((7 * 6) + 5)
+print((248 % 100) / 5)
+print((6 * 3) - (9 / 4))
+print((5 - 7) * 4)
+print(6 + (18 % (17 - 12)))
