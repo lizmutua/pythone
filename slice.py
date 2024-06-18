57fc5f022f081f5cb05f78c5d618ab69d8f56f4b
Liz Mutua <70329767+lizmutua@users.noreply.github.com>
2024-06-18 23:51:49 +0300

    slice

---
+++ slice.py
@@ -0,0 +1,20 @@
+#def half(test6):
+      #middle=len(test6)//2
+      #return test6[0:middle -1]
+#data = input("Enter a String : ")
+#print("First Half of String is : ", half(data))
+
+#def half(test7):
+   # middle=len(test7)//2
+   # return test7[middle +1:]
+#data = input("Enter a String : ")
+#print("First Half of String is : ", half(data))
+
+#x="water"
+#print(x.replace('w','c',1))
+
+#S = "What is your name?"
+#print(S[::2])
+
+S = "What is your name?"
+print(S[2:8:-1])
